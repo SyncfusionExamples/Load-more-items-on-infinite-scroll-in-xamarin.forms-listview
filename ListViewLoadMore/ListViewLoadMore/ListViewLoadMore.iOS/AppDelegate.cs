091964e924559e2635a3166ddded9bcafc999664
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2021-02-24 17:48:03 +0530

    XAMARIN-39158 Sample updated to latest nuget

--- ListViewLoadMore/ListViewLoadMore/ListViewLoadMore.iOS/AppDelegate.cs
+++ ListViewLoadMore/ListViewLoadMore/ListViewLoadMore.iOS/AppDelegate.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
+using Syncfusion.ListView.XForms.iOS;
 using UIKit;
 
 namespace ListViewLoadMore.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
-    // application events from iOS.
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
@@ -23,6 +19,7 @@ namespace ListViewLoadMore.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            SfListViewRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
--- ListViewLoadMore/ListViewLoadMore/ListViewLoadMore.iOS/Properties/AssemblyInfo.cs
+++ ListViewLoadMore/ListViewLoadMore/ListViewLoadMore.iOS/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("72bdc44f-c588-44f3-b6df-9aace7daafdd")]
+[assembly: Guid("50c7b8c9-e664-45af-b88e-0c9b8b9c1be1")]
 
 // Version information for an assembly consists of the following four values:
 //
